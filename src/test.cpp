94be255ae76704a44647bc4f5d32604d8a1f9df9
keito-isurugi <ja224kg@gmail.com>
2023-03-14 08:02:38 +0900

    3.01.数値型

--- src/test.cpp
+++ src/test.cpp
@@ -7,7 +7,20 @@ using namespace std;
 // }
 
 int main(){
-  
+  pair<string, int> p("abc", 3);
+  cout << p.first << endl;  // abc
+ 
+  p.first = "hello";
+  cout << p.first << endl;  // hello
+  cout << p.second << endl; // 3
+ 
+  p = make_pair("*", 1);
+ 
+  string s;
+  int a;
+  tie(s, a) = p;
+  cout << s << endl;  // *
+  cout << a << endl;  // 1
 }
 
   
